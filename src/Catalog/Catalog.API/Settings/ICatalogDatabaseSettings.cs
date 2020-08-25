ba8f392c128848d5020907540e2de68d2edaa39a
dimitkos <User1@DESKTOP-D6C1HHM>
2020-08-25 22:44:02 +0300

    Create the Api for catalog microservice, also implement the connection with mongo db.

---
+++ src/Catalog/Catalog.API/Settings/ICatalogDatabaseSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Catalog.API.Settings
+{
+    public interface ICatalogDatabaseSettings
+    {
+        string ConnactionString { get; set; }
+        string DatabaseName { get; set; }
+        string CollactionName { get; set; }
+    }
+}
