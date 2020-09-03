28f5ae620fa807c7d564b8e5c11daf11ed473d27
dimitkos <User1@DESKTOP-D6C1HHM>
2020-09-03 16:01:07 +0300

    Fix delete basket action method to return 200 ok with true in the body.

--- src/Basket/Basket.API/Controllers/BasketController.cs
+++ src/Basket/Basket.API/Controllers/BasketController.cs
@@ -44,7 +44,7 @@ namespace Basket.API.Controllers
             if (!deletedBasket)
                 return NotFound();
 
-            return Ok();
+            return Ok(deletedBasket);
         }
     }
 }
