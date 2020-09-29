ffea9b6c68f98109ac5d2e2f59bacd6ed66ab0d8
dimitkos <User1@DESKTOP-D6C1HHM>
2020-09-29 20:27:29 +0300

    Add rabitmq producer and create a class for the constants

---
+++ src/Common/EventBusRabbitMQ/Producer/EventBusRabbitMQProducer.cs
@@ -0,0 +1,48 @@
+﻿using EventBusRabbitMQ.Events;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace EventBusRabbitMQ.Producer
+{
+    public class EventBusRabbitMQProducer
+    {
+        private readonly IRabbitMQConnection _connection;
+
+        public EventBusRabbitMQProducer(IRabbitMQConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public void PublishBasketCheckout(string queueName, BasketCheckoutEvent publishModel)
+        {
+            using (var channel = _connection.CreateModel())
+            {
+
+                //Durable(the queue will survive a broker restart)
+                //Exclusive(used by only one connection and the queue will be deleted when that connection closes)
+                //Auto - delete(queue that has had at least one consumer is deleted when last consumer unsubscribes)
+                //Arguments(optional; used by plugins and broker - specific features such as message TTL, queue length limit, etc)
+                channel.QueueDeclare(queue: queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+                var message = JsonConvert.SerializeObject(publishModel);
+                var body = Encoding.UTF8.GetBytes(message);
+
+                IBasicProperties properties = channel.CreateBasicProperties();
+                properties.Persistent = true;
+                properties.DeliveryMode = 2;
+
+                channel.ConfirmSelect();
+                channel.BasicPublish(exchange: "", routingKey: queueName, mandatory: true, basicProperties: properties, body: body);
+                channel.WaitForConfirmsOrDie();
+
+                channel.BasicAcks += (sender, eventArgs) =>
+                {
+                    Console.WriteLine("Sent RabbitMQ");
+                    //implement ack handle
+                };
+                channel.ConfirmSelect();
+            }
+        }
+    }
+}
