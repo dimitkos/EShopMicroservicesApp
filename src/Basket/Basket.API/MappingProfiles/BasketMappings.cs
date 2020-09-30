d6559a14da32317c51816d8b9f5008247311b780
dimitkos <User1@DESKTOP-D6C1HHM>
2020-09-30 11:36:55 +0300

    Add automapper profile.

---
+++ src/Basket/Basket.API/MappingProfiles/BasketMappings.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Basket.API.Entities;
+using EventBusRabbitMQ.Events;
+
+namespace Basket.API.MappingProfiles
+{
+    public class BasketMappings : Profile
+    {
+        public BasketMappings()
+        {
+            CreateMap<BasketCheckout, BasketCheckoutEvent>().ReverseMap();
+        }
+    }
+}
