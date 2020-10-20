6d3762799f0b373784446e8501ed5b1f94d4c44f
dimitkos <User1@DESKTOP-D6C1HHM>
2020-10-20 16:27:16 +0300

    Add repositories in core layer

---
+++ src/Ordering/Ordering.Core/Repositories/IOrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using Ordering.Core.Entities;
+using Ordering.Core.Repositories.Base;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Ordering.Core.Repositories
+{
+    public interface IOrderRepository : IRepository<Order>
+    {
+        //here can add custom methods
+        Task<IEnumerable<Order>> GetOrderByUsername(string username);
+    }
+}
