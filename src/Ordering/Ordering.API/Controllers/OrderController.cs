b1f6771303fc5b825c42ef56d16604557ae08c17
dimitkos <User1@DESKTOP-D6C1HHM>
2020-11-02 23:15:55 +0200

    Implement Irder Controller

--- src/Ordering/Ordering.API/Controllers/OrderController.cs
+++ src/Ordering/Ordering.API/Controllers/OrderController.cs
@@ -1,4 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Ordering.Application.Commands;
+using Ordering.Application.Queries;
+using Ordering.Application.Responses;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
 
 namespace Ordering.API.Controllers
 {
@@ -6,5 +13,29 @@ namespace Ordering.API.Controllers
     [Route("api/v1/order")]
     public class OrderController : ControllerBase
     {
+        private readonly IMediator _mediator;
+
+        public OrderController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<OrderResponse>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<OrderResponse>>> GetOrdersByUsername(string username)
+        {
+            var orders = await _mediator.Send(new GetOrdersByUserNameQuery(username));
+
+            return Ok(orders);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(OrderResponse), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<OrderResponse>> CheckoutOrder([FromBody] CheckoutOrderCommand command)
+        {
+            var order = await _mediator.Send(command);
+
+            return Ok(order);
+        }
     }
 }
