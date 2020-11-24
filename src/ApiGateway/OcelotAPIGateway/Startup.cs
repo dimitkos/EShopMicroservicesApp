f8b5ce490312849b9074779aa62f5fcec4aed442
dimitkos <User1@DESKTOP-D6C1HHM>
2020-11-24 11:49:59 +0200

    SetUp ocelot and implement json file to config ocelot

--- src/ApiGateway/OcelotAPIGateway/Startup.cs
+++ src/ApiGateway/OcelotAPIGateway/Startup.cs
@@ -1,25 +1,21 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 
 namespace OcelotAPIGateway
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddOcelot();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public async void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -35,6 +31,9 @@ namespace OcelotAPIGateway
                     await context.Response.WriteAsync("Hello World!");
                 });
             });
+
+            //sometimes may await a little bit time for this reason we use async await 
+            await app.UseOcelot();
         }
     }
 }
