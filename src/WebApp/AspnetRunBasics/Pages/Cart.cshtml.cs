efffeb6afd824c85343897a69840581068272f9b
dimitkos <User1@DESKTOP-D6C1HHM>
2020-11-25 12:02:58 +0200

    Implement web application

---
+++ src/WebApp/AspnetRunBasics/Pages/Cart.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using AspnetRunBasics.ApiCollection.Interfaces;
+using AspnetRunBasics.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspnetRunBasics
+{
+    public class CartModel : PageModel
+    {
+        private readonly IBasketApi _basketApi;
+
+        public CartModel(IBasketApi basketApi)
+        {
+            _basketApi = basketApi;
+        }
+
+        public BasketModel Cart { get; set; } = new BasketModel();
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            Cart = await _basketApi.GetBasket("swn");
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostRemoveToCartAsync(string productId)
+        {
+            var basket = await _basketApi.GetBasket("swn");
+
+            var item = basket.Items.Single(x => x.ProductId == productId);
+            basket.Items.Remove(item);
+
+            await _basketApi.UpdateBasket(basket);
+
+            return RedirectToPage();
+        }
+    }
+}
---
+++ src/WebApp/AspnetRunBasics/Pages/CheckOut.cshtml.cs
@@ -0,0 +1,49 @@
+﻿using AspnetRunBasics.ApiCollection.Interfaces;
+using AspnetRunBasics.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
+
+namespace AspnetRunBasics
+{
+    public class CheckOutModel : PageModel
+    {
+        private readonly ICatalogApi _catalogApi;
+        private readonly IBasketApi _basketApi;
+
+        public CheckOutModel(ICatalogApi catalogApi, IBasketApi basketApi)
+        {
+            _catalogApi = catalogApi;
+            _basketApi = basketApi;
+        }
+
+        [BindProperty]
+        public BasketCheckoutModel Order { get; set; }
+
+        public BasketModel Cart { get; set; } = new BasketModel();
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            Cart = await _basketApi.GetBasket("swn");
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostCheckOutAsync()
+        {
+            Cart = await _basketApi.GetBasket("swn");
+
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            Order.UserName = "swn";
+            Order.TotalPrice = Cart.TotalPrice;
+
+            await _basketApi.CheckoutBasket(Order);
+
+            return RedirectToPage("Confirmation", "OrderSubmitted");
+        }
+    }
+}
